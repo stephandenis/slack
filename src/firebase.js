25228ad7db8001c0aa49490877d7090d70bb5eb4
Stephan <stephangdenis@gmail.com>
2021-06-19 03:28:16 -0300

    newComponent[Header]

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Fg5eJ-j5SzvUQaF6jaufvZEOZSyGv3Q",
+  authDomain: "slack-deni.firebaseapp.com",
+  projectId: "slack-deni",
+  storageBucket: "slack-deni.appspot.com",
+  messagingSenderId: "180545656211",
+  appId: "1:180545656211:web:769eee403e66efbd408466",
+};
